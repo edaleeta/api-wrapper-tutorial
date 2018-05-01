f811e06be82ba1b39c8d4bf4bd2f9637776fde9a
Edelita Valdez <edaleeta@gmail.com>
2018-05-01 12:14:23 -0700

    Creates fixture for expected response keys; uses fixture in test.

--- tests/test_tmdbwrapper.py
+++ tests/test_tmdbwrapper.py
@@ -1,6 +1,17 @@
+from pytest import fixture
 from tmdbwrapper import TV
 
-def test_tv_info():
+
+@fixture
+def tv_keys():
+    #  Responsible only for returning the test data.
+    keys = ["id", "origin_country", "poster_path", "name",
+            "overview", "popularity", "backdrop_path", "first_air_date",
+            "vote_count", "vote_average"]
+    return keys
+
+
+def test_tv_info(tv_keys):
     """Tests an API call to get a TV show's info."""
 
     tv_instance = TV(63247)
@@ -8,3 +19,5 @@ def test_tv_info():
 
     assert isinstance(response, dict)
     assert response["id"] == 63247, "The ID should be in the response."
+    assert set(tv_keys).issubset(response.keys()), (
+           "All keys should be in the response.")
